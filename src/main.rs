1631c5230856d48a515aea3f5f9b075978eb3990
Igor Semyonov <igor@semyonov.xyz>
2024-02-19 09:55:10 -0500

    forward pass working

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,16 @@ struct LeakyIntegrateAndFireNeuron {
     u: f64, //membrane potential
     beta: f64,
     u_threshold: f64,
+    u_min: f64,
 }
 impl LeakyIntegrateAndFireNeuron {
     fn new(
         u: f64,
         beta: f64,
         u_threshold: f64,
+        u_min: f64,
     ) -> Self {
-        Self { u, beta, u_threshold }
+        Self { u, beta, u_threshold, u_min }
     }
 
     fn step(
@@ -29,6 +31,7 @@ impl LeakyIntegrateAndFireNeuron {
         let mut spike_out = 0.0;
         if self.u >= self.u_threshold {
             u_next -= self.u_threshold;
+            u_next = u_next.max(self.u_min);
             spike_out = 1.0;
         }
         let (u_next, spike_out) =
@@ -61,17 +64,23 @@ impl Network {
 struct Simulation {
     network: Network,
     memory: nd::Array2<f64>, // [i, j] is potential of the jth neuron at time i
-    spikes: nd::Array2<f64>, // spikes [i, j] is the spike amount received by the jth neuron at i
+    spikes_in: nd::Array2<f64>, // spikes [i, j] is the spike amount received by the jth neuron at i
+    spikes_out: nd::Array2<f64>, // spikes [i, j] is the spike amount produced by the jth neuron at i
     n_steps: usize,
     i_step: usize,
 }
 impl Simulation {
     fn new(
         network: Network,
-        spikes: nd::Array2<f64>,
+        spikes_in: nd::Array2<f64>,
         n_steps: usize,
     ) -> Self {
         let mut memory =
+            nd::Array2::zeros([
+                n_steps,
+                network.n_neurons(),
+            ]);
+        let spikes_out =
             nd::Array2::zeros([
                 n_steps,
                 network.n_neurons(),
@@ -84,22 +93,22 @@ impl Simulation {
         Self {
             network,
             memory,
-            spikes,
+            spikes_in,
+            spikes_out,
             n_steps,
             i_step,
         }
     }
     fn step(self: &mut Self) {
-        let spikes_current = &self
-            .spikes
+        let current_spikes_in = self
+            .spikes_in
             .slice(nd::s![self.i_step, ..])
-            .dot(&self.network.weights);
-        dbg!(spikes_current);
+            .into_owned();
         self.network
             .neurons
             .clone()
             .into_iter()
-            .zip(spikes_current)
+            .zip(current_spikes_in)
             .enumerate()
             .for_each(
                 |(
@@ -108,10 +117,9 @@ impl Simulation {
                 )| {
                     let (
                         memory_next,
-                        spike_next,
-                    ) = neuron.step(
-                        *spike_current,
-                    );
+                        spike_out_next,
+                    ) = neuron
+                        .step(spike_current);
                     self.network.neurons
                         [idx]
                         .u = memory_next;
@@ -119,6 +127,19 @@ impl Simulation {
                         self.i_step + 1,
                         idx,
                     ]] = memory_next;
+                    for jdx in 0..self
+                        .network
+                        .n_neurons()
+                    {
+                        self.spikes_in[[
+                            self.i_step + 1,
+                            jdx,
+                        ]] += spike_out_next
+                            * self
+                                .network
+                                .weights
+                                [[idx, jdx]];
+                    }
                     // let mut
                     // spikes_next_slice = self
                     //     .spikes
@@ -152,28 +173,30 @@ impl Simulation {
 }
 
 fn main() {
-    let n_steps = 20usize;
+    let n_steps = 30usize;
 
     let neurons = (0..=1)
         .map(|_| {
             LeakyIntegrateAndFireNeuron::new(
-                1.0, 0.98, 1.5,
+                1.0, 0.9, 1.5, 0.2,
             )
         })
         .collect::<Vec<_>>();
 
     let weights =
-        nd::arr2(&[[1.0, 0.5], [0.5, 1.0]]);
+        nd::arr2(&[[0.0, 1.5], [0.5, 0.0]]);
     let network =
         Network::new(neurons, weights);
-    let mut spikes = nd::Array2::zeros([
+    let mut spikes_in = nd::Array2::zeros([
         n_steps,
         network.n_neurons(),
     ]);
-    spikes[[10, 0]] = 1.0;
+
+    spikes_in[[5, 0]] = 1.0;
+    // spikes_in[[12, 1]] = 1.0;
 
     let mut simulation = Simulation::new(
-        network, spikes, n_steps,
+        network, spikes_in, n_steps,
     );
     simulation.run();
     // dbg!(&simulation);
